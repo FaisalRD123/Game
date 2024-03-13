f444137f78f0f7d4ae7da120834001d0aa6680fc
FaisalRD123 <faisalazizrd33@gmail.com>
2024-03-13 06:47:51 +0000

    Created a function that will launch right at the start with the help of window.onload and created a loop to show images

--- rockPaperScizzor.js
+++ rockPaperScizzor.js
@@ -2,3 +2,11 @@ let me, rival;
 let myPoint = 0, rivalPoint = 0;
 
 let options = ['rock', 'paper', 'scizzor'];
+
+window.onload = function () {  /* Function for putting images on the screen */
+    for (let i = 0; i <= 2; i++) {  /* Array for images*/
+        let option = document.createElement('img');
+        option.id = options[i];
+        option.src = options[i] + '.png';
+    }
+}
